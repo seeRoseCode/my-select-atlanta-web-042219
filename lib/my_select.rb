4f56a6bfadb097afa5d328eefc33707c8d6cbccf
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-19 23:11:15 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+    i = 0
+    select = []
+    while i < array.length
+      if yield(array[i])
+        select << array[i]
+      end
+      i+=1
+    end
+    select
+  end
 end
